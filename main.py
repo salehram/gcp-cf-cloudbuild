2cb36d676a821cd40b1958bd697f2a366cdc980f
Saleh Ramadan <salehram@users.noreply.github.com>
2023-05-21 22:45:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import mysql.connector
 
 def return_basic(request):
   now = datetime.now()
-  return 'Welcome to Cloud Functions and Cloud Build demo - ' + str(now)
+  return '<h1>Welcome to Cloud Functions and Cloud Build demo - ' + str(now) + '</h1>'
